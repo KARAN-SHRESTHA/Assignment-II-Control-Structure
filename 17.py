4aa483640e4f12ed21ae7a8480f6f7709ffdafeb
Karan Shrestha <48661099+KARAN-SHRESTHA@users.noreply.github.com>
2020-12-14 19:14:43 +0545

    Update 17.py

--- 17.py
+++ 17.py
@@ -8,7 +8,7 @@ op = input("Enter Operator:  ")
 value = 0
 
 if op == '/':
-    if first_num == 0 or second_num:
+    if first_num == 0 or second_num == 0:
         print("0 Value error")
     else:
         print(first_num/second_num)
